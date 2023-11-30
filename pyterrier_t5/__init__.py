8ff32df3263ee1c807c62cbd70669d5eba0d8b01
Lukas Zeit-Altpeter <lukas.zeit-altpeter@uni-jena.de>
2023-11-30 11:48:52 +0100

    move device selection to its own function

--- pyterrier_t5/__init__.py
+++ pyterrier_t5/__init__.py
@@ -14,23 +14,28 @@ from typing import List
 import re
 
 
+def get_best_device():
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+    return device
+
+
 class MonoT5ReRanker(TransformerBase):
-    def __init__(self, 
-                 tok_model='t5-base',
-                 model='castorini/monot5-base-msmarco',
-                 batch_size=4,
-                 text_field='text',
-                 verbose=True):
+    def __init__(
+        self,
+        tok_model="t5-base",
+        model="castorini/monot5-base-msmarco",
+        batch_size=4,
+        text_field="text",
+        verbose=True,
+    ):
         self.verbose = verbose
         self.batch_size = batch_size
-
-        if torch.cuda.is_available():
-            device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
-        else:
-            device = "cpu"
-
+        device = get_best_device()
         self.device = torch.device(device)
         self.tokenizer = T5Tokenizer.from_pretrained(tok_model)
         self.model_name = model
@@ -38,60 +43,71 @@ class MonoT5ReRanker(TransformerBase):
         self.model.to(self.device)
         self.model.eval()
         self.text_field = text_field
-        self.REL = self.tokenizer.encode('true')[0]
-        self.NREL = self.tokenizer.encode('false')[0]
+        self.REL = self.tokenizer.encode("true")[0]
+        self.NREL = self.tokenizer.encode("false")[0]
 
     def __str__(self):
         return f"MonoT5({self.model_name})"
 
     def transform(self, run):
         scores = []
-        queries, texts = run['query'], run[self.text_field]
+        queries, texts = run["query"], run[self.text_field]
         it = range(0, len(queries), self.batch_size)
-        prompts = self.tokenizer.batch_encode_plus([f'Relevant:' for _ in range(self.batch_size)], return_tensors='pt', padding='longest')
-        max_vlen = self.model.config.n_positions - prompts['input_ids'].shape[1]
+        prompts = self.tokenizer.batch_encode_plus(
+            [f"Relevant:" for _ in range(self.batch_size)],
+            return_tensors="pt",
+            padding="longest",
+        )
+        max_vlen = self.model.config.n_positions - prompts["input_ids"].shape[1]
         if self.verbose:
-            it = pt.tqdm(it, desc='monoT5', unit='batches')
+            it = pt.tqdm(it, desc="monoT5", unit="batches")
         for start_idx in it:
-            rng = slice(start_idx, start_idx+self.batch_size) # same as start_idx:start_idx+self.batch_size
-            enc = self.tokenizer.batch_encode_plus([f'Query: {q} Document: {d}' for q, d in zip(queries[rng], texts[rng])], return_tensors='pt', padding='longest')
+            rng = slice(
+                start_idx, start_idx + self.batch_size
+            )  # same as start_idx:start_idx+self.batch_size
+            enc = self.tokenizer.batch_encode_plus(
+                [f"Query: {q} Document: {d}" for q, d in zip(queries[rng], texts[rng])],
+                return_tensors="pt",
+                padding="longest",
+            )
             for key, enc_value in list(enc.items()):
-                enc_value = enc_value[:, :-1] # chop off end of sequence token-- this will be added with the prompt
-                enc_value = enc_value[:, :max_vlen] # truncate any tokens that will not fit once the prompt is added
-                enc[key] = torch.cat([enc_value, prompts[key][:enc_value.shape[0]]], dim=1) # add in the prompt to the end
-            enc['decoder_input_ids'] = torch.full(
+                enc_value = enc_value[
+                    :, :-1
+                ]  # chop off end of sequence token-- this will be added with the prompt
+                enc_value = enc_value[
+                    :, :max_vlen
+                ]  # truncate any tokens that will not fit once the prompt is added
+                enc[key] = torch.cat(
+                    [enc_value, prompts[key][: enc_value.shape[0]]], dim=1
+                )  # add in the prompt to the end
+            enc["decoder_input_ids"] = torch.full(
                 (len(queries[rng]), 1),
                 self.model.config.decoder_start_token_id,
-                dtype=torch.long
+                dtype=torch.long,
             )
             enc = {k: v.to(self.device) for k, v in enc.items()}
             with torch.no_grad():
                 result = self.model(**enc).logits
             result = result[:, 0, (self.REL, self.NREL)]
             scores += F.log_softmax(result, dim=1)[:, 0].cpu().detach().tolist()
-        run = run.drop(columns=['score', 'rank'], errors='ignore').assign(score=scores)
+        run = run.drop(columns=["score", "rank"], errors="ignore").assign(score=scores)
         run = add_ranks(run)
         return run
 
 
 class DuoT5ReRanker(TransformerBase):
-    def __init__(self, 
-                 tok_model='t5-base',
-                 model='castorini/duot5-base-msmarco',
-                 batch_size=4,
-                 text_field='text',
-                 verbose=True,
-                 agg='sum'):
+    def __init__(
+        self,
+        tok_model="t5-base",
+        model="castorini/duot5-base-msmarco",
+        batch_size=4,
+        text_field="text",
+        verbose=True,
+        agg="sum",
+    ):
         self.verbose = verbose
         self.batch_size = batch_size
-
-        if torch.cuda.is_available():
-            device = "cuda"
-        elif torch.backends.mps.is_available():
-            device = "mps"
-        else:
-            device = "cpu"
-
+        device = get_best_device()
         self.device = torch.device(device)
         self.tokenizer = T5Tokenizer.from_pretrained(tok_model)
         self.model_name = model
